e4d6145be78f5a0e5eab89660dbf32c915e90c5f
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2022-11-12 21:58:51 -0600

    feat: added eda script and plot

---
+++ analytics/utils.py
@@ -0,0 +1,25 @@
+"""
+author:Sanidhya Mangal
+github:sanidhyamangal
+"""
+import pandas as pd # for dataframe based loading
+from datetime import datetime
+import matplotlib.pyplot as plt # for plotting
+from typing import Tuple
+
+def load_dataset(file_path:str):
+    # data = np.loadtxt(file_path, sep="::")
+    df = pd.read_csv(file_path, sep="::",header=None,names=["buyer", "item", "seller", "timestamp"])
+    df['timestamp'] = df['timestamp'].apply(lambda x: datetime.fromtimestamp(x).strftime("%Y-%m-%d"))
+
+    return df
+
+def plot_degree_dist(graph, plot_path:str,plot_name:str,xlim:Tuple[int]=(0,50)):
+    degrees = [graph.degree(n) for n in graph.nodes()]
+
+    plt.hist(degrees, bins="auto")
+    plt.xlim(*xlim)
+    plt.xlabel("Degrees")
+    plt.ylabel("Freq")
+    plt.title(plot_name)
+    plt.savefig(plot_path)
