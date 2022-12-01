b97ce41d623c74ca1ee62d5a1ea2722a14214026
npotteig <npottei1@umbc.edu>
2022-12-01 13:04:06 -0600

    fixed: root part to be dynamic in world

--- code/world.py
+++ code/world.py
@@ -16,7 +16,7 @@ import multiprocessing
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 args = parse_args()
 
-ROOT_PATH = "PUT_YOUR_PATH_TO_HERE"
+ROOT_PATH = os.path.split(os.path.abspath(__name__))[0]
 CODE_PATH = join(ROOT_PATH, 'code')
 DATA_PATH = join(ROOT_PATH, 'data')
 BOARD_PATH = join(CODE_PATH, 'runs')
