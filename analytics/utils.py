1546af3419791e47ba5dcb7a433e980856b98689
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2022-12-02 11:44:14 -0600

    reformat: yapf

--- analytics/utils.py
+++ analytics/utils.py
@@ -2,19 +2,29 @@
 author:Sanidhya Mangal
 github:sanidhyamangal
 """
-import pandas as pd # for dataframe based loading
 from datetime import datetime
-import matplotlib.pyplot as plt # for plotting
 from typing import Tuple
 
-def load_dataset(file_path:str):
+import matplotlib.pyplot as plt  # for plotting
+import pandas as pd  # for dataframe based loading
+
+
+def load_dataset(file_path: str):
     # data = np.loadtxt(file_path, sep="::")
-    df = pd.read_csv(file_path, sep="::",header=None,names=["buyer", "item", "seller", "timestamp"])
-    df['timestamp'] = df['timestamp'].apply(lambda x: datetime.fromtimestamp(x).strftime("%Y-%m-%d"))
+    df = pd.read_csv(file_path,
+                     sep="::",
+                     header=None,
+                     names=["buyer", "item", "seller", "timestamp"])
+    df['timestamp'] = df['timestamp'].apply(
+        lambda x: datetime.fromtimestamp(x).strftime("%Y-%m-%d"))
 
     return df
 
-def plot_degree_dist(graph, plot_path:str,plot_name:str,xlim:Tuple[int]=(0,50)):
+
+def plot_degree_dist(graph,
+                     plot_path: str,
+                     plot_name: str,
+                     xlim: Tuple[int] = (0, 50)):
     degrees = [graph.degree(n) for n in graph.nodes()]
 
     plt.hist(degrees, bins="auto")
--- eda.py
+++ eda.py
@@ -3,30 +3,29 @@ author:Sanidhya Mangal
 github:sanidhyamangal
 """
 
-from analytics.utils import load_dataset, plot_degree_dist
+import matplotlib.pyplot as plt  # for plotting
 import networkx as nx
-import matplotlib.pyplot as plt # for plotting
 
-
+from analytics.utils import load_dataset, plot_degree_dist
 
 df_ebid = load_dataset("data/ebid_buyer_item_seller_id_R_2.txt")
-
 
 G_bi = nx.from_pandas_edgelist(df_ebid, "buyer", "item")
 G_is = nx.from_pandas_edgelist(df_ebid, "item", "seller")
 G_bs = nx.from_pandas_edgelist(df_ebid, "buyer", "seller")
 
-plot_degree_dist(G_is,"ebid_item_seller_deg.png","Item Seller Degree Dist")
-plot_degree_dist(G_bi,"ebid_buyer_item_deg.png","Buyer Item Degree Dist")
-plot_degree_dist(G_bs,"ebid_buyer_seller_deg.png","Buyer Seller Degree Dist")
+plot_degree_dist(G_is, "ebid_item_seller_deg.png", "Item Seller Degree Dist")
+plot_degree_dist(G_bi, "ebid_buyer_item_deg.png", "Buyer Item Degree Dist")
+plot_degree_dist(G_bs, "ebid_buyer_seller_deg.png", "Buyer Seller Degree Dist")
 
 df_bonanza = load_dataset("data/bonanza_buyer_item_seller_id_R_2.txt")
-
 
 G_bi = nx.from_pandas_edgelist(df_bonanza, "buyer", "item")
 G_is = nx.from_pandas_edgelist(df_bonanza, "item", "seller")
 G_bs = nx.from_pandas_edgelist(df_bonanza, "buyer", "seller")
 
-plot_degree_dist(G_is,"bonanza_item_seller_deg.png","Item Seller Degree Dist")
-plot_degree_dist(G_bi,"bonanza_buyer_item_deg.png","Buyer Item Degree Dist")
-plot_degree_dist(G_bs,"bonanza_buyer_seller_deg.png","Buyer Seller Degree Dist")
+plot_degree_dist(G_is, "bonanza_item_seller_deg.png",
+                 "Item Seller Degree Dist")
+plot_degree_dist(G_bi, "bonanza_buyer_item_deg.png", "Buyer Item Degree Dist")
+plot_degree_dist(G_bs, "bonanza_buyer_seller_deg.png",
+                 "Buyer Seller Degree Dist")
